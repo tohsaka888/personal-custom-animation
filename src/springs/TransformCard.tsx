b38a0ff2ce144af183c230951f83ad9636c77e1a
tohsaka888 <156132264@qq.com>
2021-11-30 14:43:18 +0800

    增加可配置项和默认值

--- src/springs/TransformCard.tsx
+++ src/springs/TransformCard.tsx
@@ -5,23 +5,36 @@ type Props = {
   children: ReactElement;
   isMove: boolean;
   configs?: SpringConfig;
+  initialX?: number;
+  transformX?: number;
+  delay?: number;
 };
 
-export default function TransformCard({ children, isMove, configs }: Props) {
+export default function TransformCard({
+  children,
+  isMove,
+  configs,
+  initialX = 100,
+  transformX = 0,
+  delay = 0,
+}: Props) {
   const styles = useSpring({
     from: {
-      transform: isMove ? "translate3d(100%,0,0)" : "translate3d(0%,0,0)",
+      transform: isMove
+        ? `translate3d(${initialX}%,0,0)`
+        : `translate3d(${transformX}%,0,0)`,
     },
     to: isMove
       ? async (next) => {
           await next({ display: "block" });
-          await next({ transform: "translate3d(0%,0,0)" });
+          await next({ transform: `translate3d(${transformX}%,0,0)` });
         }
       : async (next) => {
-          await next({ transform: "translate3d(100%,0,0)" });
+          await next({ transform: `translate3d(${initialX}%,0,0)` });
           await next({ display: "none" });
         },
     config: configs || config.gentle,
+    delay: delay,
   });
   return <animated.div style={styles}>{children}</animated.div>;
 }
