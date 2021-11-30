4fd2dd9ff55d0fcb392233cf1f7671a23230bd89
tohsaka888 <156132264@qq.com>
2021-11-30 14:15:35 +0800

    增加实例

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import TextMovement from "./springs/TextMovement";
+import TransformCard from "./springs/TransformCard";
+import { SpringConfig } from "react-spring";
+
+const configs: SpringConfig = {
+  mass: 1,
+  tension: 200,
+  friction: 20,
+  bounce: 0.6,
+};
 
 function App() {
   const [isMove, setIsMove] = useState<boolean>(false);
@@ -13,9 +22,26 @@ function App() {
       >
         show?
       </button>
-      <TextMovement isMove={isMove}>
+      <TextMovement
+        isMove={isMove}
+        initivalFontSize="1rem"
+        transformFontSize="5rem"
+      >
         <div>111111</div>
       </TextMovement>
+      <TransformCard isMove={isMove} configs={configs}>
+        <div
+          style={{
+            height: "30vh",
+            width: "90vw",
+            background: "red",
+            marginLeft: "5vw",
+            borderRadius: "20px",
+          }}
+        >
+          <div style={{ padding: "40px" }}>card</div>
+        </div>
+      </TransformCard>
     </div>
   );
 }
