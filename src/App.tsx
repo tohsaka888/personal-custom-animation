afd24dad3795281abb860a08dc998319be8c6a69
tohsaka888 <156132264@qq.com>
2021-11-30 14:43:01 +0800

    实例更新

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,18 @@ import TextMovement from "./springs/TextMovement";
 import TransformCard from "./springs/TransformCard";
 import { SpringConfig } from "react-spring";
 
-const configs: SpringConfig = {
+const configsLeft: SpringConfig = {
   mass: 1,
   tension: 200,
-  friction: 20,
-  bounce: 0.6,
+  friction: 50,
+  bounce: 0.8,
+};
+
+const configsRight: SpringConfig = {
+  mass: 1,
+  tension: 200,
+  friction: 30,
+  bounce: 0.8,
 };
 
 function App() {
@@ -22,25 +29,23 @@ function App() {
       >
         show?
       </button>
+      <TransformCard isMove={isMove} configs={configsLeft}>
+        <div className="card">card</div>
+      </TransformCard>
       <TextMovement
         isMove={isMove}
         initivalFontSize="1rem"
         transformFontSize="5rem"
       >
-        <div>111111</div>
+        <div style={{ textAlign: "center" }}>Hello,我是Tohsaka888</div>
       </TextMovement>
-      <TransformCard isMove={isMove} configs={configs}>
-        <div
-          style={{
-            height: "30vh",
-            width: "90vw",
-            background: "red",
-            marginLeft: "5vw",
-            borderRadius: "20px",
-          }}
-        >
-          <div style={{ padding: "40px" }}>card</div>
-        </div>
+      <TransformCard
+        isMove={isMove}
+        configs={configsRight}
+        initialX={-100}
+        delay={200}
+      >
+        <div className="card">card</div>
       </TransformCard>
     </div>
   );
