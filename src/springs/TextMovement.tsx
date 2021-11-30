71d73e6d67c22b459508dd47e95ad1cf211fe13c
tohsaka888 <156132264@qq.com>
2021-11-30 13:35:23 +0800

    可定义动画转变前后的字体大小

--- src/springs/TextMovement.tsx
+++ src/springs/TextMovement.tsx
@@ -4,23 +4,30 @@ import { animated, useSpring, config } from "react-spring";
 type Props = {
   children: ReactElement;
   isMove: boolean;
+  initivalFontSize: string;
+  transformFontSize: string;
 };
 
-export default function TextMovement({ children, isMove }: Props): JSX.Element {
+export default function TextMovement({
+  children,
+  isMove,
+  initivalFontSize,
+  transformFontSize,
+}: Props): JSX.Element {
   const styles = useSpring({
     from: {
       display: isMove ? "none" : "block",
       transform: isMove ? "translate3d(100%,0,0)" : "translate3d(0%,0,0)",
-      fontSize: isMove ? "3rem" : "10rem",
+      fontSize: isMove ? initivalFontSize : transformFontSize,
     },
     to: isMove
       ? async (next) => {
           await next({ display: "block" });
           await next({ transform: "translate3d(0%,0,0)" });
-          await next({ fontSize: "10rem" });
+          await next({ fontSize: transformFontSize });
         }
       : async (next) => {
-          await next({ fontSize: "3rem" });
+          await next({ fontSize: initivalFontSize });
           await next({ transform: "translate3d(100%,0,0)" });
           await next({ display: "none" });
         },
