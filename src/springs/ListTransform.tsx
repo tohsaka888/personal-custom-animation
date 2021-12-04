b9295e99fd6877591c21953ddaaf76d0cb571865
tohsaka888 <156132264@qq.com>
2021-12-04 15:50:13 +0800

    增加列表移动动画

--- src/springs/ListTransform.tsx
+++ src/springs/ListTransform.tsx
@@ -1,20 +1,21 @@
-import React, { ReactElement } from "react";
+import React, { ReactNode } from "react";
 import { useTrail, animated } from "react-spring";
 
-type Item = {
-  title: string;
-  content: string;
-  showDate?: boolean;
-  tags: string[];
-  intro: string;
-};
+// type Item = {
+//   title: string;
+//   content: string;
+//   showDate?: boolean;
+//   tags: string[];
+//   intro: string;
+// };
 
 type Props = {
   isMove: boolean;
-  items: any[];
+  children: ReactNode;
 };
 
-export default function ListTransform({ items, isMove }: Props) {
+export default function ListTransform({ isMove, children }: Props) {
+  const items = React.Children.toArray(children);
   const trails = useTrail(items.length, {
     from: isMove
       ? { transfrom: "translate3d(100%,0,0)" }
@@ -27,18 +28,7 @@ export default function ListTransform({ items, isMove }: Props) {
     <>
       {trails.map((style, index) => {
         const item = items[index];
-        return (
-          <animated.div style={style}>
-            <div>
-              <h1>{item.title || "实例"}</h1>
-              {item.tags &&
-                item.tags.map((value: string, index: number) => {
-                  return <div key={index}>{value}</div>;
-                })}
-              <div>{item.content || "这是一段介绍......"}</div>
-            </div>
-          </animated.div>
-        );
+        return <animated.div style={style}>{item}</animated.div>;
       })}
     </>
   );
